4ec9e79ab8b8005de1ee13d19ba515d12306b378
Jonmorais <jonathan_ms15@hotmail.com>
2021-06-09 19:56:17 -0300

    First fieldset done

---
+++ trybe-exercices/6.1/script.js
@@ -0,0 +1,37 @@
+let statesBr = [
+  'Acre',
+  'Alagoas',
+  'Amapá',
+  'Amazonas',
+  'Bahia',
+  'Ceará',
+  'Distrito Federal',
+  'Espírito Santo',
+  'Goiás',
+  'Maranhão',
+  'Mato Grosso',
+  'Mato Grosso do Sul',
+  'Minas Gerais',
+  'Pará',
+  'Paraíba',
+  'Paraná',
+  'Pernambuco',
+  'Piauí',
+  'Rio de Janeiro',
+  'Rio Grande do Norte',
+  'Rio Grande do Sul',
+  'Rondônia',
+  'Roraima',
+  'Santa Catarina',
+  'São Paulo',
+  'Sergipe',
+  'Tocantins',
+];
+
+const selectState = document.querySelector('#select-state');
+
+for(let i = 0; i < statesBr.length; i += 1) {
+  const stateOption = document.createElement('option');
+  stateOption.innerHTML = statesBr[i];
+  selectState.appendChild(stateOption);
+}
